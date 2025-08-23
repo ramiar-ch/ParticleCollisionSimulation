7853a879ee575b73581d903397cc3c672025b89f
Ramiar Chowdhury <ramiar.chowdhury@gmail.com>
2025-08-23 18:38:04 +0100

    Add project files.

---
+++ ParticleSim/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticleSim
+{
+    public partial class StartMenu : Form
+    {
+        private Dictionary<Button, Func<Form>> navigationMap;
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}

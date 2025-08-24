40c0a68d0d679abf4d5ffd2fc17a353f69559e4c
Ramiar Chowdhury <ramiar.chowdhury@gmail.com>
2025-08-24 20:59:00 +0100

    Help page designed

--- ParticleSim/StartMenu.Designer.cs
+++ ParticleSim/StartMenu.Designer.cs
@@ -145,7 +145,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
-            this.ClientSize = new System.Drawing.Size(668, 453);
+            this.ClientSize = new System.Drawing.Size(662, 453);
             this.Controls.Add(this.btnHelpGuide);
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.titleMenu);
